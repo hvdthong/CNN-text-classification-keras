246adc954b5449276b0f70578aff66200a376b48
hvdthong <hvdthong@github.com>
2018-08-15 11:45:28 +0800

    adding data helper and model

--- model.py
+++ model.py
@@ -14,7 +14,7 @@ x, y, vocabulary, vocabulary_inv = load_data()
 # len(vocabulary) -> 18765
 # len(vocabulary_inv) -> 18765
 
-X_train, X_test, y_train, y_test = train_test_split( x, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
 
 # X_train.shape -> (8529, 56)
 # y_train.shape -> (8529, 2)
@@ -22,10 +22,10 @@ X_train, X_test, y_train, y_test = train_test_split( x, y, test_size=0.2, random
 # y_test.shape -> (2133, 2)
 
 
-sequence_length = x.shape[1] # 56
-vocabulary_size = len(vocabulary_inv) # 18765
+sequence_length = x.shape[1]  # 56
+vocabulary_size = len(vocabulary_inv)  # 18765
 embedding_dim = 256
-filter_sizes = [3,4,5]
+filter_sizes = [3, 4, 5]
 num_filters = 512
 drop = 0.5
 
@@ -36,15 +36,18 @@ batch_size = 30
 print("Creating Model...")
 inputs = Input(shape=(sequence_length,), dtype='int32')
 embedding = Embedding(input_dim=vocabulary_size, output_dim=embedding_dim, input_length=sequence_length)(inputs)
-reshape = Reshape((sequence_length,embedding_dim,1))(embedding)
+reshape = Reshape((sequence_length, embedding_dim, 1))(embedding)
 
-conv_0 = Conv2D(num_filters, kernel_size=(filter_sizes[0], embedding_dim), padding='valid', kernel_initializer='normal', activation='relu')(reshape)
-conv_1 = Conv2D(num_filters, kernel_size=(filter_sizes[1], embedding_dim), padding='valid', kernel_initializer='normal', activation='relu')(reshape)
-conv_2 = Conv2D(num_filters, kernel_size=(filter_sizes[2], embedding_dim), padding='valid', kernel_initializer='normal', activation='relu')(reshape)
+conv_0 = Conv2D(num_filters, kernel_size=(filter_sizes[0], embedding_dim), padding='valid', kernel_initializer='normal',
+                activation='relu')(reshape)
+conv_1 = Conv2D(num_filters, kernel_size=(filter_sizes[1], embedding_dim), padding='valid', kernel_initializer='normal',
+                activation='relu')(reshape)
+conv_2 = Conv2D(num_filters, kernel_size=(filter_sizes[2], embedding_dim), padding='valid', kernel_initializer='normal',
+                activation='relu')(reshape)
 
-maxpool_0 = MaxPool2D(pool_size=(sequence_length - filter_sizes[0] + 1, 1), strides=(1,1), padding='valid')(conv_0)
-maxpool_1 = MaxPool2D(pool_size=(sequence_length - filter_sizes[1] + 1, 1), strides=(1,1), padding='valid')(conv_1)
-maxpool_2 = MaxPool2D(pool_size=(sequence_length - filter_sizes[2] + 1, 1), strides=(1,1), padding='valid')(conv_2)
+maxpool_0 = MaxPool2D(pool_size=(sequence_length - filter_sizes[0] + 1, 1), strides=(1, 1), padding='valid')(conv_0)
+maxpool_1 = MaxPool2D(pool_size=(sequence_length - filter_sizes[1] + 1, 1), strides=(1, 1), padding='valid')(conv_1)
+maxpool_2 = MaxPool2D(pool_size=(sequence_length - filter_sizes[2] + 1, 1), strides=(1, 1), padding='valid')(conv_2)
 
 concatenated_tensor = Concatenate(axis=1)([maxpool_0, maxpool_1, maxpool_2])
 flatten = Flatten()(concatenated_tensor)
@@ -54,12 +57,11 @@ output = Dense(units=2, activation='softmax')(dropout)
 # this creates a model that includes
 model = Model(inputs=inputs, outputs=output)
 
-checkpoint = ModelCheckpoint('weights.{epoch:03d}-{val_acc:.4f}.hdf5', monitor='val_acc', verbose=1, save_best_only=True, mode='auto')
+checkpoint = ModelCheckpoint('weights.{epoch:03d}-{val_acc:.4f}.hdf5', monitor='val_acc', verbose=1,
+                             save_best_only=True, mode='auto')
 adam = Adam(lr=1e-4, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
 
 model.compile(optimizer=adam, loss='binary_crossentropy', metrics=['accuracy'])
 print("Traning Model...")
-model.fit(X_train, y_train, batch_size=batch_size, epochs=epochs, verbose=1, callbacks=[checkpoint], validation_data=(X_test, y_test))  # starts training
-
-
-
+model.fit(X_train, y_train, batch_size=batch_size, epochs=epochs, verbose=1, callbacks=[checkpoint],
+          validation_data=(X_test, y_test))  # starts training
